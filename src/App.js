3abeea9a313411d163ea67bd7dce3a0adb34f112
kateacraig <kate.a.craig@gmail.com>
2025-11-22 21:48:50 -0500

    Added GitHub repo url

--- src/App.js
+++ src/App.js
@@ -3,7 +3,28 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">Weather App</header>
+      <div className="App-header">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/kateacraig"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {" "}
+            Kate Craig
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/kateacraig/react-weather-app-project"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
