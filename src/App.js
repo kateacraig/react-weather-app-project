f9a936446402386f725c0dda56baadad0eafcb20
kateacraig <kate.a.craig@gmail.com>
2025-11-22 22:14:43 -0500

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,26 +4,28 @@ export default function App() {
   return (
     <div className="App">
       <div className="App-header">
-        <h1>Weather App</h1>
-        <footer>
-          This project was coded by{" "}
-          <a
-            href="https://github.com/kateacraig"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {" "}
-            Kate Craig
-          </a>{" "}
-          and is{" "}
-          <a
-            href="https://github.com/kateacraig/react-weather-app-project"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            open-sourced on GitHub
-          </a>
-        </footer>
+        <div className="container">
+          <h1>Weather App</h1>
+          <footer>
+            This project was coded by{" "}
+            <a
+              href="https://github.com/kateacraig"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {" "}
+              Kate Craig
+            </a>{" "}
+            and is{" "}
+            <a
+              href="https://github.com/kateacraig/react-weather-app-project"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              open-sourced on GitHub
+            </a>
+          </footer>
+        </div>
       </div>
     </div>
   );
