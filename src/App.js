4defb51bca150ab53eed74930d50eb28dd4332cf
kateacraig <kate.a.craig@gmail.com>
2025-11-22 22:39:46 -0500

    Added html, form, and class names

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
@@ -6,6 +7,7 @@ export default function App() {
       <div className="App-header">
         <div className="container">
           <h1>Weather App</h1>
+          <Weather />
           <footer>
             This project was coded by{" "}
             <a
