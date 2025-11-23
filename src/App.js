db4c70306776fdc937278c1f26a625d55d752b0d
kateacraig <kate.a.craig@gmail.com>
2025-11-22 23:39:25 -0500

    added css

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ export default function App() {
     <div className="App">
       <div className="App-header">
         <div className="container">
-          <h1>Weather App</h1>
           <Weather />
           <footer>
             This project was coded by{" "}
