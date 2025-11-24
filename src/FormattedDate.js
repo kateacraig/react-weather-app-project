73b3731cce6fc9cdb2feeaa608a9193b9c4783b5
kateacraig <kate.a.craig@gmail.com>
2025-11-23 20:36:49 -0500

    Updated hours

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -12,9 +12,6 @@ export default function FormattedDate(props) {
   ];
   let day = days[props.date.getDay()];
   let hours = props.date.getHours();
-  if (hours > 12) {
-    hours = `${hours - 12}`;
-  }
   let minutes = props.date.getMinutes();
   if (minutes < 10) {
     minutes = `0${minutes}`;
